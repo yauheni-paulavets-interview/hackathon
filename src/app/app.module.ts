db30d9329acb6e50a7d3687bae57672a7c4768e4
Yauheni Paulavets <yauheni.paulavets@coople.com>
2019-02-05 22:35:17 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
